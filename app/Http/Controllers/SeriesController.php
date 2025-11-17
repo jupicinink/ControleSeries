8dded1dfc55f76efff00792541fbe4a1a8fbb9a9
jupicinink <juliapicinini.2020@gmail.com>
2025-11-17 15:42:29 -0300

    Implement series management functionality with CRUD operations and database migration

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use App\Models\Serie;
+
 
 use Illuminate\Http\Request;
 
@@ -8,16 +11,25 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
+        $series = Serie::query()->orderBy('nome')->get();
+
         return view('series.index')->with('series', $series);
     }
 
     public function create(){
         return view('series.create');
+    }
+
+    // vantagem: é muito sipple de usar tendo somente uma classe
+    // desvantagem: a modelagem do dominio fica menos espressiva
+    public function store(Request $request){
+        $nomeSerie = $request->input('nome');
+        // pego o nome da série do formulário e salvo no banco de dados
+        //Serie é um modelo que representa a tabela series no banco de dados
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        return redirect('/series');
     }
 
 }
